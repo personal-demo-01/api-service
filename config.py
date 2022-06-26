1fcfbbb35667ac589ef9b778885b4bf792a15862
novac-dev <devetest7733@gmail.com>
2022-06-25 23:03:54 -0400

    Create Dockerfile

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
-    TESTING = False
+    TESTING = True
     CSRF_ENABLED = True
     #Database URL
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    #SQLALCHEMY_DATABASE_URI = 'mysql://root:mypass@192.168.33.10:3306/demo_01'
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
