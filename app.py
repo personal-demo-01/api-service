96cfa0a6b0ae8e98625028dda24e7518ed98a60c
novac-dev <devetest7733@gmail.com>
2022-06-26 23:32:24 -0400

    Update api version

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from models import Client
 #Index
 @app.route("/")
 def index():
-    return "This is the first api endpoint. TEST 002"
+    return "This is the first api endpoint. TEST 003"
 
 #Add client and money
 @app.route("/add")
